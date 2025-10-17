8a81f5c15c15c42e9d7dc88f37dc299a36e513fe
Maicxl09 <rendongonzalezmaicol2@gmail.com>
2025-10-17 09:01:10 -0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener ('DOMContentLoaded', function () {
+    const navToggle = document.getElementById('navToggle');
+    const navMenu = document.getElementById('navMenu');
+    const navToggleLines = document.querySelectorAll('.nav__toggle-line');
+
+    //Evento click para el boton hamburguesa
+    navToggle.addEventListener('click', function(){
+        // Alternar clase activa en el menu
+        navMenu.classList.toggle('nav__menu--active');
+
+        // Alternar clases activas en las lineas del hamburguesa
+        navToggleLines[0].classList.toggle('nav__toggle-line--top-active');
+        navToggleLines[0].classList.toggle('nav__toggle-line--middle-active');
+        navToggleLines[0].classList.toggle('nav__toggle-line--bottom-active');
+    });
+});
